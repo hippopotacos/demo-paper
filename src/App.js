0f9d5c95d3b63dff228541343835a8b6f2b2969e
hippopotacos <hippopotacos@gmail.com>
2021-01-10 23:29:44 +0900

    textareaで入力した内容を別の箇所に反映・レイアウト調整

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import paper from './paper.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ''
+    }
+    this.doChange = this.doChange.bind(this);
+  }
+
+  doChange(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  render(){
+    return(
+      <div className="wrapper">
+        <div className="wrapper__inner">
+          <h1>paper app</h1>
+          <div>ここに出力テスト→{this.state.text}</div>
+          <textarea className="textarea" placeholder="ここに入力してください" value={this.state.text} onChange={this.doChange}></textarea>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
